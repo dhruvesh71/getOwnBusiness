334845dd59f7ba0ba4677af143cd758857748464
dhruvesh <dhruvesh71@gmail.com>
2020-05-10 19:18:17 +0530

    Css Feature added

--- src/app/components/business/getownbusiness/getownbusiness.component.ts
+++ src/app/components/business/getownbusiness/getownbusiness.component.ts
@@ -11,10 +11,36 @@ export class GetownbusinessComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    new WOW(  {  live: false}).init();
+    new WOW().init();
   }
 
   scrollToPage(page: HTMLElement) {
     page.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  @HostListener('document:scroll', ['$event'])
+  documentScroll(event: any) {
+
+    var header = document.getElementById("header");
+    var home = document.getElementById("home");
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition >= 480) {
+
+      header.style.backgroundColor = "#fff";
+      header.style.color = "#000";
+      header.style.position = "fixed";
+      header.style.borderBottom = "1px solid rgba(0, 0, 0, 0.2)";
+      header.style.fontSize = "8px";
+
+    } else {
+      header.style.position = "absolute";
+      header.style.backgroundColor = "transparent";
+      header.style.color = "#fff";
+      header.style.borderBottom = "none";
+      header.style.fontSize = "20px";
+
+    }
+    home.style.transform = `translate3d(0px, ${scrollPosition/3}px, 0px)`;
   }
 }
